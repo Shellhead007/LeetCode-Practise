404f71e1291716cec06e46f21c45dd6bc54c365a
Abhimanyu <sharmaabhimanyu07@gmail.com>
2021-01-11 23:29:19 +0530

    92 ms

--- solutions/1302. Deepest Leaves Sum.cpp
+++ solutions/1302. Deepest Leaves Sum.cpp
@@ -11,10 +11,20 @@
  */
 class Solution {
 public:
+    int max_depth = 0;
     int deepestLeavesSum(TreeNode* root) {
         if(!root) return 0;
-        int ans = 0;
-        
-        return ans;
+        max_depth = depth(root);
+        return deepUtil(root,0);
+    }
+private :
+    int depth(TreeNode* root) {
+        if(!root) return 0;
+        return 1 + max(depth(root->left),depth(root->right));
+    }    
+    int deepUtil(TreeNode* root, int d) {
+        if(!root) return 0;
+        if(d == max_depth - 1) return root->val;
+        return deepUtil(root->left,d+1)+deepUtil(root->right,d+1); 
     }
 };
